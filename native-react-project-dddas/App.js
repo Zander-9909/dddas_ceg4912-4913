24093769d7c3ad2c2156154bd0a45684b41cba4d
Kenneth <71233549+boryukenneth@users.noreply.github.com>
2023-03-09 09:30:17 -0500

    set up directories and screen files

--- native-react-project-dddas/App.js
+++ native-react-project-dddas/App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 
-export default function App() {
+export default function App(props) {
   return (
-    <View style={styles.container}>
-      <Text>This is the front end for the DDDAS project in Native React.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#66ccff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
