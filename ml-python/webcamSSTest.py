059f2c800f197af5e3969c5e0ae0557d9a3ff5d6
djtoastman14 <71289356+djtoastman14@users.noreply.github.com>
2023-03-24 10:05:04 -0400

    small fil name issue
    changed colons to dashes so windows can make the file

--- ml-python/webcamSSTest.py
+++ ml-python/webcamSSTest.py
@@ -4,9 +4,10 @@ from datetime import datetime
 
 camera = cv2.VideoCapture(0)#enable camera[0] as a capture device
 framerate = camera.get(5) * 0.5 #Get half the frame rate
-path = "/home/zander/CEG4912-3/dddas_ceg4912-4913/ml-python/frames/"
+path = "/Users/RyeTo/Documents/GitHub/dddas_ceg4912-4913/ml-python/frames"
+i = 0
 
-while(camera.isOpened()):
+while(i < 100):
     framenum = camera.get(1) #get current frame number
     return_value, image = camera.read()
     if(return_value != True):
@@ -14,9 +15,13 @@ while(camera.isOpened()):
     if(framenum % math.floor(framerate) == 0):
         now = datetime.now()
 
-        current_time = now.strftime("%H:%M:%S")
+        current_time = now.strftime("%H.%M.%S")
 
         name = path + "/frame" + current_time+ ".png"
+        print(name)
         cv2.imwrite(name, image)
         print("Captured at :" +current_time)
-del(camera)
+        i+=1
+
+camera.release()
+cv2.destroyAllWindows()
