2e4ca35e3d22946b8165bda68685fdfdbe2d40df
boryukenneth <chenbykenneth@hotmail.com>
2023-10-19 02:52:41 -0400

    For Demo
    values are hardcoded for demonstration purposes, node modules don't work for some reason very annoying!

--- native-react-project-dddas/screens/MapScreen.js
+++ native-react-project-dddas/screens/MapScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
 
 const MapScreen = () => {
     return (
         <SafeAreaView>
-            <Text>Here is the map stuff</Text>
+            <Text style={tw`text-3xl font-bold p-8`}>Mapping Screen</Text>
         </SafeAreaView>
 
     )
